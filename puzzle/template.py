033b030efb46e7eee00730d009c5632bc30c7310
marques-j-robinson <marques.j.robinson@gmail.com>
2022-07-09 13:08:46 -0700

    create template generation

---
+++ puzzle/template.py
@@ -0,0 +1,10 @@
+from puzzle.input import get_puzzle_input
+
+
+if __name__ == '__main__':
+    pass
+    # Test Data
+    # x = get_puzzle_input(EVENT, DAY)
+
+    # print(f'part 1:\n0')
+    # print(f'part 2:\n0')
